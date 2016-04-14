5a74a9298ee2b2e3f6f4e5867709c7bd761a00ca
Rafał Soboń <rafal.sobon@excodus.com>
2016-04-14 12:40:06 +0200

    improved browser language detection, added locale cookie handling

--- routes.php
+++ routes.php
@@ -8,6 +8,7 @@
 
 use RainLab\Translate\Classes\Translator;
 use RainLab\Translate\Models\Locale;
+use Cookie;
 
 App::before(function($request) {
     $translator = Translator::instance();
@@ -15,16 +16,34 @@ App::before(function($request) {
         return;
 
     $locale = Request::segment(1);
-    if ($locale != post('locale')) {
+
+    /*
+     * Behavior when changing locale from the locale picker; post('locale') has priority over $locale,
+     * because Request still have old locale in the URL, hence $locale is outdated and User sends new locale in the POST
+     * TODO: hook the translate plugin's onSwitchLocale ajax handler instead of checking on post
+     */
+    if (post('locale') && $locale != post('locale')) {
         $translator->setLocale(post('locale'));
+        Cookie::queue('locale', post('locale'), 10080);
     }
+    /*
+     * Behavior when there is no locale in the Request URL, first check cookies and then try to match with default browser language
+     */
     if (!$locale) {
-        // get the browser language
-        $accepted = parseLanguageList($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-        $available = Locale::listEnabled();
-        $matches = findMatches($accepted, $available);
-        $match = array_values($matches)[0][0];
-        $translator->setLocale($match);
+        $cookieLocale = Cookie::get('locale');
+        if ($cookieLocale) {
+            $translator->setLocale($cookieLocale);
+        } else {
+            // get the list of browser languages
+            $accepted = parseLanguageList($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+            $available = Locale::listEnabled();
+            // match against languages enabled in Translate plugin
+            // TODO: allow october backend users to create their own mappings to the locale short codes
+            $matches = findMatches($accepted, $available);
+            // get the first match
+            $match = array_values($matches)[0];
+            $translator->setLocale($match);
+        }
     }
 
     $locale = $translator->getLocale();
@@ -51,6 +70,9 @@ App::before(function($request) {
 
 });
 
+// browser language parser based on Gumbo's answer
+// http://stackoverflow.com/a/3771447/3704886
+
 // parse list of comma separated language tags and sort it by the quality value
 function parseLanguageList($languageList) {
     if (is_null($languageList)) {
@@ -69,9 +91,8 @@ function parseLanguageList($languageList) {
                 $match[2] = (string) floatval($match[2]);
             }
             if (!isset($languages[$match[2]])) {
-                $languages[$match[2]] = array();
+                $languages[$match[2]] = strtolower($match[1]);
             }
-            $languages[$match[2]][] = strtolower($match[1]);
         }
     }
     krsort($languages);
@@ -82,23 +103,18 @@ function parseLanguageList($languageList) {
 function findMatches($accepted, $available) {
     $matches = array();
     $any = false;
-    foreach ($accepted as $acceptedQuality => $acceptedValues) {
+    foreach ($accepted as $acceptedQuality => $acceptedValue) {
         $acceptedQuality = floatval($acceptedQuality);
         if ($acceptedQuality === 0.0) continue;
         foreach ($available as $key => $value) {
-            foreach ($acceptedValues as $acceptedValue) {
-                if ($acceptedValue === '*') {
-                    $any = true;
-                }
-                $matchingGrade = matchLanguage($acceptedValue, $key);
-                if ($matchingGrade > 0) {
-                    $q = (string) ($acceptedQuality * $matchingGrade);
-                    if (!isset($matches[$q])) {
-                        $matches[$q] = array();
-                    }
-                    if (!in_array($key, $matches[$q])) {
-                        $matches[$q][] = $key;
-                    }
+            if ($acceptedValue === '*') {
+                $any = true;
+            }
+            $matchingGrade = matchLanguage($acceptedValue, $key);
+            if ($matchingGrade > 0) {
+                $q = (string) ($acceptedQuality * $matchingGrade);
+                if (!in_array($q, $matches)) {
+                    $matches[$q] = $key;
                 }
             }
         }
@@ -111,11 +127,13 @@ function findMatches($accepted, $available) {
 }
 
 // compare two language tags and distinguish the degree of matching
+// edit: actually matching "en-us" with "en" will always return "1"
 function matchLanguage($a, $b) {
     $a = explode('-', $a);
     $b = explode('-', $b);
     for ($i=0, $n=min(count($a), count($b)); $i<$n; $i++) {
         if ($a[$i] !== $b[$i]) break;
     }
-    return $i === 0 ? 0 : (float) $i / count($a);
+//    return $i === 0 ? 0 : (float) $i / count($a);
+    return $i;
 }
